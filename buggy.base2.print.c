d1f0d884f1e55337a6a90d903151bf27370f0d56
106b <lunarbuffalomoon@gmail.com>
2025-04-02 14:00:41 -0400

    Fixed Bugs
    --Format Specifier for printfs were wrong. Changed %d to %c
    --Loop condition was wrong and was not terminating. Changed !(k=0) to (k>=0)

--- buggy.base2.print.c
+++ buggy.base2.print.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main ( ) {
-	unsigned int numToPrintInBase2 = 1431655765;	/* alternating 1's and 0's */
+	//unsigned int numToPrintInBase2 = 1431655765;	/* alternating 1's and 0's */
+	int numToPrintInBase2 = 7;
 	unsigned int exp = 1;
 	int k;	/* can't declare variable in a loop header */
 	/* Compute the highest storable power of 2 (2 to the 31th). */
@@ -9,7 +10,7 @@ int main ( ) {
 	}
 	/* For each power of 2 from the highest to the lowest,
 		print 1 if it occurs in the number, 0 otherwise. */
-	for (k=31; !(k=0); k--) {
+	for (k=31; (k>=0); k--) {
 		if (numToPrintInBase2 >= exp) {
 			printf ("%c", '1');
 			numToPrintInBase2 = numToPrintInBase2 - exp;
@@ -22,5 +23,7 @@ int main ( ) {
 	return 0;
 }
 
-//printfs were originally printing character representations of 1 and 0, but format specifer was 
+//-printfs were originally printing character representations of 1 and 0, but format specifer was 
 // %d. Changed to %c. Alternate change is get rid of quotes around 1 and 0.
+//-The loop condition in the for loop was all wrong, changed to (k>=0)
+//I believe that solves it
