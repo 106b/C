bf9728d1ab0e657c520f13e2643f33859c9ab890
myn <lunarbuffalomoon@gmail.com>
2025-04-23 22:30:10 -0400

    How?:save place in string and keep j from updating

--- entab.c
+++ entab.c
@@ -1,24 +1,42 @@
-//program to replace spaces with equal numbers of tabs
+//program to replace strings of blanks with equal numbers of tabs and blanks
 #include <stdio.h>
 #define LIM 1024
-
+#define IN 1
+#define OUT 0
+#define TAB 4
 int getl(char line[], int lim);
 
 int main(){
 	int len;
-	char line[LIM];
+	char line[LIM] = "";
 
 	while((len = getl(line, LIM)) > 0){
-		printf("%s\n", line);
-		printf("%d\n", len);
+		printf("%s", line);
+		printf("%d\n", len-1);
 	}
 }
 int getl(char line[], int lim){
 	char c;
 	int i;
+	int blankCount = 0;
+	//if OUT of a word AND c is a space
+	//start counting the spaces
+	//if spaceCount == TAB, replace string of spaces with '\t'
 	for(i = 0; (c = getchar()) != EOF && c != '\n' && i < lim-1; i++){
-		line[i] = c;
-		
+		if(c == ' '){
+			int j = i; //save place in string
+			blankCount++;
+			if(blankCount == TAB){
+				line[j] = '\t';
+				i = j; // move back in string;
+				blankCount = 0; //reset counter
+				printf("tab added\n");
+			} else {
+				line[i] = c;
+			}
+		} else {
+			line[i] = c;
+		}		
 	}
 	if(c == '\n'){
 		line[i] = '\n';
@@ -26,8 +44,6 @@ int getl(char line[], int lim){
 	}
 	line[i] = '\0';
 	return i;
-
-
 }
 
 
