8790d6a004bf9a343a172ad16bf1b7c3f1b2556b
myn <lunarbuffalomoon@gmail.com>
2025-05-01 22:43:43 -0400

    starting htoi() program

---
+++ hextointeger.c
@@ -0,0 +1,16 @@
+//write a function htoi(s) that converts string of hex into integer equivalent
+#include <stdio.h>
+#define LIM 33
+void htoi(char hexstring[]);
+int main(){
+	char s[LIM] = "";
+	printf("enter hex string: ");
+	scanf("%s", s);;
+	printf("%s", s);
+
+
+}
+
+void htoi(char hexstring[]);
+
+
