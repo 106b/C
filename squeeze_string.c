5c62ba4446fe923277f69c9230eac34142b2376b
myn <lunarbuffalomoon@gmail.com>
2025-05-07 22:21:21 -0400

    added printf debug

--- squeeze_string.c
+++ squeeze_string.c
@@ -1,6 +1,7 @@
 //remove all occurences of chars in string 2 from string 1
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define LIM 32
 void squeeze(char s1[], char s2[]);
 int main(){
@@ -23,13 +24,17 @@ void squeeze(char s1[], char s2[]){
 
 	int i, j, k;
 
-	for(k = 0; s2[k] != '\0'; k++){
-		for(i = j = 0; s1[i] != '\0'; i++){
-			if(s1[i] != s2[k]){
-				s1[j++] = s1[i];
+	for(i = 0; s2[i] != '\0'; i++){
+		printf("deleting %c\n", s2[i]);
+		for(j = k = 0; s1[j] != '\0'; j++){
+			if(s1[j] != s2[i]){
+				s1[k++] = s1[j];
 			}
+			printf("%s\n", s1);
 		}
+
 	}
-	s1[i] = '\0';
+
+	s1[j] = '\0';
 
 }
