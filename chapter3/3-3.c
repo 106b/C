f53188446022fc10e18acc96602f02bba0d1d6eb
mock <lunarbuffalomoon@gmail.com>
2025-05-23 09:22:59 -0400

    buggy expansion

--- chapter3/3-3.c
+++ chapter3/3-3.c
@@ -1,5 +1,7 @@
 // Given a format like a-z in s1, write expansion abc..z in s2
 #include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
 #define LIMIT 80
 
 void expansion (char str1[], char str2[]);
@@ -10,15 +12,32 @@ int main(){
 
 	while(fgets(string1, LIMIT - 1, stdin) != NULL && string1[0]  != EOF){
 		expansion(string1, string2);
-		printf("%s", string2);
+		printf("%s\n", string2);
+		string2[0] = '\0';
 	}
 }
 
 
 void expansion (char str1[], char str2[]){
+	bool expansion = false;
+    char start = '\0';
+	int j = 0;
 
-	for(int i = 0; str[i] != '\0'; i++){
-		
+	 for(int i = 0; str1[i] != '\0'; i++, j++)
+	{
+		if (!expansion)
+		{	
+			expansion = true;
+			start = str1[i];
+			str2[j] = str1[i];
+		} else 
+		{
+			expansion = false;
+			for(; j < (LIMIT - 1) && start < str1[i]; j++)
+			{
+				str2[j] = ++start;
+			}
+		}
 	}
-
+	 str2[j++] = '\0';
 }
